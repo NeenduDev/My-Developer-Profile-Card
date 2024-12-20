fc436d74e942d4dc1598cba59337e67f068d5949
NeenduDev <neendu.wickremasinghe@gmail.com>
2024-12-20 19:49:17 +0530

    Complete initial attempt

---
+++ src/index.js
@@ -0,0 +1,112 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="Neendu.JPG" alt="Anjitha Neendu" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Anjitha Neendu</h1>
+      <p>
+        Self-taught full-stack web developer passionate about building impactful
+        web solutions. When I'm not coding, you'll find me singing, strumming my
+        guitar, or exploring the great outdoors to connect with nature.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+      ))}
+      {/* <Skill skill="React" emoji="💪" color="#123456" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orangered" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Svelte" emoji="💪" color="orange" /> */}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+// function Skill(props) {
+//   // I could use Prop Destructuring like the above one.
+//   return (
+//     <div className="skill" style={{ backgroundColor: props.color }}>
+//       <span>{props.skill}</span>
+//       <span>{props.emoji}</span>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
